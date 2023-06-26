e2bb502d73bf279c5d3b1a64534fbe462fcf751f
Timur Turianytsia <turianytsia.timur@gmail.com>
2023-06-26 11:46:56 +0300

    feat: Added basic security

--- backend-java/src/main/java/com/taru/taskmanager/repository/UserRepository.java
+++ backend-java/src/main/java/com/taru/taskmanager/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.taru.taskmanager.repository;
 import com.taru.taskmanager.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
