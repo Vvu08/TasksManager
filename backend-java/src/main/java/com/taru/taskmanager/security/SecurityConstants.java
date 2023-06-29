48bb1fd3983e4dc2633bfa3b1fbb0113d4d5a0e0
Timur Turianytsia <turianytsia.timur@gmail.com>
2023-06-29 11:58:55 +0300

    refactor: Changed the expiration date of the JWT token

--- backend-java/src/main/java/com/taru/taskmanager/security/SecurityConstants.java
+++ backend-java/src/main/java/com/taru/taskmanager/security/SecurityConstants.java
@@ -6,7 +6,7 @@ import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 9000000;
+    public static final long JWT_EXPIRATION = 90000000;
     public static final Key KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     public static String CURRENT_TOKEN;
 }
